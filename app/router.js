a27327e100ec8b8f44526f5853f3a7e34b79867b
Amos Chan <amosschan@gmail.com>
2014-12-24 13:10:31 -0500

    Initial commit of group.bulletin resource

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('group', { path: 'groups/:group_id' }, function() { });
+  this.resource('group/bulletin', { path: 'group/bulletins/:group/bulletin_id' }, function() { });
 });
 
 export default Router;
