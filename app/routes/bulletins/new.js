3d6f0ffe2ad53d49f291d2f0a4a7c4317842a562
Amos Chan <amosschan@gmail.com>
2015-01-06 22:16:33 -0500

    Set the default publishedAt to the upcoming Sunday

--- app/routes/bulletins/new.js
+++ app/routes/bulletins/new.js
@@ -1,12 +1,23 @@
 import Ember from 'ember';
 
+function upcomingSunday() {
+  return moment().endOf('week')
+                 .add(1, 'day')
+                 .hours(9)
+                 .minutes(30)
+                 .seconds(0)
+                 .milliseconds(0);
+}
+
 export default Ember.Route.extend({
   model: function() {
+    var publishedAt = upcomingSunday();
     var englishService = this.store.find('group', 1);
+
     return this.store.createRecord('bulletin', {
-      publishedAt: new Date(),
+      publishedAt: publishedAt.toDate(),
       name: 'Sunday Worship Service',
-      description: 'Default description',
+      description: publishedAt,
       serviceOrder: 'Default service order',
       group: englishService
     });
--- tests/unit/routes/bulletins/new-test.js
+++ tests/unit/routes/bulletins/new-test.js
@@ -5,32 +5,33 @@ import {
   test
 } from 'ember-qunit';
 
+var englishService, newBulletin;
+
 moduleFor('route:bulletins/new', 'BulletinsNewRoute', {
   // Specify the other units that are required for this test.
-  needs: ['model:bulletin']
+  needs: ['model:bulletin'],
+  setup: function() {
+    englishService = {
+      id: 1,
+      name: 'English Service',
+      slug: 'english-service'
+    };
+
+    newBulletin = {
+      id: 1,
+      name: 'Sunday Worship Service',
+      description: 'Default Desc',
+      serviceOrder: 'Service Order',
+      group: englishService
+    };
+  }
 });
 
 test('it populates model with a new bulletin', function() {
-  expect(5);
-
-  var container = new Ember.Container();
-  container.register('store:main', DS.Store);
+  expect(11);
 
   var route = this.subject();
-  var englishService = {
-    id: 1,
-    name: 'English Service',
-    slug: 'english-service'
-  };
-  var newBulletin = {
-    id: 1,
-    name: 'Sunday Worship Service',
-    description: 'Default Desc',
-    serviceOrder: 'Service Order',
-    group: englishService
-  };
-
-  var mockStore = {
+  route.store = {
     find: function(model, id) {
       equal(model, 'group');
       equal(id, 1);
@@ -41,11 +42,17 @@ test('it populates model with a new bulletin', function() {
       equal(model, 'bulletin');
       equal(objectHash.group, englishService);
 
+      // default publishedAt is a Sunday, 9:30am
+      equal(objectHash.publishedAt.getDay(), 0);
+      equal(objectHash.publishedAt.getHours(), 9);
+      equal(objectHash.publishedAt.getMinutes(), 30);
+      equal(objectHash.publishedAt.getSeconds(), 0);
+      equal(objectHash.publishedAt.getMilliseconds(), 0);
+      ok(objectHash.publishedAt.getTime() > new Date().getTime());
+
       return newBulletin;
     }
   };
-
-  route.store = mockStore;
 
   equal(route.model(), newBulletin);
 });
