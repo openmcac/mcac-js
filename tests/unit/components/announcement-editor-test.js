21a74424e7b1733d53ec9a32874cb07318f69c63
Amos Chan <amosschan@gmail.com>
2015-01-24 22:07:40 -0500

    Add tests for BulletinsNewAnnouncementsController

--- tests/unit/components/announcement-editor-test.js
+++ tests/unit/components/announcement-editor-test.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import Announcement from 'mcac/models/announcement';
 import {
   moduleForComponent,
   test
--- tests/unit/controllers/bulletins/new/announcements-test.js
+++ tests/unit/controllers/bulletins/new/announcements-test.js
@@ -1,15 +1,80 @@
-import {
-  moduleFor,
-  test
-} from 'ember-qunit';
+import FactoryGuy from 'factory-guy';
+import { testMixin as FactoryGuyTestMixin } from 'factory-guy';
+import Ember from 'ember';
+import { moduleFor, test } from 'ember-qunit';
+import startApp from '../../../../helpers/start-app';
 
-moduleFor('controller:bulletins/new/announcements', 'BulletinsNewAnnouncementsController', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+var application,
+    testHelper,
+    englishService,
+    controller,
+    TestHelper = Ember.Object.createWithMixins(FactoryGuyTestMixin);
+
+moduleFor('controller:bulletins/new/announcements',
+          'BulletinsNewAnnouncementsController', {
+  needs: ['model:bulletin'],
+  setup: function() {
+    application = startApp();
+    testHelper = TestHelper.setup(application);
+
+    controller = this.subject();
+    controller.model = testHelper.make('bulletin');
+    controller.store = testHelper.getStore();
+
+    Ember.run(function() {
+      controller.model.get('announcements').
+                       addObjects(FactoryGuy.makeList('announcement', 3));
+    });
+  },
+  teardown: function() {
+    Ember.run(application, 'destroy');
+  }
 });
 
-// Replace this with your real tests.
-test('it exists', function() {
-  var controller = this.subject();
-  ok(controller);
+test('addAnnouncement - it adds announcement at index specified', function() {
+  Ember.run(function() {
+    controller.send('addAnnouncement', 1);
+  });
+
+  var announcements = controller.model.get('announcements');
+
+  equal(announcements.objectAt(1).get('id'), null);
+  equal(announcements.get('length'), 4);
+});
+
+test("addAnnouncement - it syncs all the announcements' positions", function() {
+  Ember.run(function() {
+    controller.send('addAnnouncement', 1);
+  });
+
+  var announcements = controller.model.get('announcements');
+
+  equal(announcements.objectAt(0).get('position'), 1);
+  equal(announcements.objectAt(1).get('position'), 2);
+  equal(announcements.objectAt(2).get('position'), 3);
+  equal(announcements.objectAt(3).get('position'), 4);
+});
+
+test('removeAnnouncement - it removes announcement at index specified', function() {
+  var announcements = controller.model.get('announcements');
+  var announcementAfterDeletedAnnouncement = announcements.objectAt(2);
+
+  Ember.run(function() {
+    controller.send('removeAnnouncement', 1);
+  });
+
+  equal(announcements.objectAt(1), announcementAfterDeletedAnnouncement);
+  equal(announcements.get('length'), 2);
+});
+
+test("removeAnnouncement - it syncs all the announcements' positions", function() {
+  Ember.run(function() {
+    controller.send('removeAnnouncement', 0);
+  });
+
+  var announcements = controller.model.get('announcements');
+
+  equal(announcements.objectAt(0).get('position'), 1);
+  equal(announcements.objectAt(1).get('position'), 2);
+  equal(announcements.get('length'), 2);
 });
