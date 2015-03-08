34f0affdee0657537ccd0caa8fd0e54cc56cbcb1
Amos Chan <amosschan@gmail.com>
2015-03-07 23:39:48 -0500

    Changes to work with actual basechurch server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,5 +5,7 @@ export default JsonApiAdapter.extend({
   headers: {
     'Accept': 'application/vnd.api+json',
     'Content-Type': 'application/vnd.api+json',
+    'X-User-Email': 'test@example.com',
+    'X-User-Token': 'cb4c2535050a6b6ca4f8155ea2882ef3'
   }
 });
