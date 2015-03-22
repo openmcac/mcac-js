d235947b4899898cd6886e0cfd998a92df817d34
Amos Chan <amosschan@gmail.com>
2015-03-22 17:45:10 -0400

    Staging application deployment
    $ ember deploy --environment staging

---
+++ config/deploy.js
@@ -0,0 +1,20 @@
+/* jshint node: true */
+
+module.exports = {
+  staging: {
+    store: {
+      type: 'redis',
+      host: 'staging.mcac.church',
+      port: 6379
+    },
+    assets: {
+      gzip: false,
+      type: 'rsync',
+      dest: 'deploy@staging.mcac.church:~/apps/mcac-js',
+      ssh: true,
+      port: 22,
+      privateKey: '~/.ssh/id_rsa',
+      args: ['-av']
+    }
+  }
+};
