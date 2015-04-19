a258e20156aebb9eb4ef6fdc777e6302fb0db61c
Amos Chan <amosschan@gmail.com>
2015-04-18 20:52:34 -0400

    View bulletins
    Moves code from bulletin.sunday.index to bulletin.index and render
    bulletin.index in bulletin.sunday.index.
    
    The BulletinSundayRoute now fetches the current bulletin from the
    /sunday endpoint and then renders bulletin.index with it.

--- app/views/bulletin/index.js
+++ app/views/bulletin/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  templateName: 'bulletin/sunday/index',
+  templateName: 'bulletin/index',
   didInsertElement: function() {
     this._super();
 
