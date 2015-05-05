3b4e1334c0c6932d724de8cce8f7a97e7a99c11c
Joseph <jyo.lewis@gmail.com>
2015-05-04 21:55:14 -0400

    Adds failing unit-test for logging on Enter.

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -66,6 +66,18 @@ function createServer() {
   return server;
 }
 
+test('logging in by pressing enter on the password field', function(assert) {
+  visit('/login');
+
+  fillIn('.email', 'test@example.com');
+  fillIn('.password', 'loginpass');
+  keyEvent('.password', 'keypress', 13);
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sunday');
+  });
+});
+
 test('logging in by clicking the submit button', function(assert) {
   visit('/login');
 
