132ce157e916ef30635580b472def2fe1d3dfa6f
Amos Chan <amosschan@gmail.com>
2015-05-04 20:19:40 -0400

    Refactor login specs to have reusable server

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -1,69 +1,73 @@
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
+import mockServer from '../helpers/server';
 import Pretender from 'pretender';
 import { module, test } from 'qunit';
 
-var application;
+var application, server;
 
 module('Acceptance: Login', {
   setup: function() {
     application = startApp();
+    server = createServer();
   },
   teardown: function() {
+    server.shutdown();
     Ember.run(application, 'destroy');
   }
 });
 
-test('visiting /login', function(assert) {
-  visit('/login');
+function createServer() {
+  var server = mockServer();
+  server.post('/api/users/sign_in', function(request) {
+    var body = JSON.parse(request.requestBody);
 
-  Ember.run(function() {
-    var server = new Pretender(function() {
-      this.post('/api/users/sign_in', function(request) {
-        var body = JSON.parse(request.requestBody);
+    if (body.user.email !== 'test@example.com' ||
+        body.user.password !== 'loginpass') {
+      return;
+    }
 
-        if (body.user.email !== 'test@example.com' ||
-            body.user.password !== 'loginpass') {
-          return;
-        }
+    var response = {
+      users: {
+        email: 'test@example.com',
+        apiKey: 'myapikey'
+      }
+    };
 
-        var response = {
-          users: {
-            email: 'test@example.com',
-            apiKey: 'myapikey'
-          }
-        };
-
-        return [
-          200,
-          {"Content-Type": "application/vnd.api+json"},
-          JSON.stringify(response)
-        ];
-      });
-
-      this.get('/api/v1/sunday', function(request) {
-        var response = {
-          bulletins: {
-            "id": 1,
-            "publishedAt": "2014-12-21T13:58:27-05:00",
-            "name": "Sunday Service",
-            "serviceOrder": "This is the service order.",
-            "description": "This is a service bulletin.",
-            "links": {
-              "group": "1",
-              "announcements": []
-            }
-          }
-        };
-
-        return [
-          200,
-          {"Content-Type": "application/vnd.api+json"},
-          JSON.stringify(response)
-        ];
-      });
-    });
+    return [
+      200,
+      {"Content-Type": "application/vnd.api+json"},
+      JSON.stringify(response)
+    ];
   });
+
+  server.get('/api/v1/sunday', function(request) {
+    var response = {
+      bulletins: {
+        "id": 1,
+        "publishedAt": "2014-12-21T13:58:27-05:00",
+        "name": "Sunday Service",
+        "serviceOrder": "This is the service order.",
+        "description": "This is a service bulletin.",
+        "links": {
+          "group": "1",
+          "announcements": []
+        }
+      }
+    };
+
+    return [
+      200,
+      {"Content-Type": "application/vnd.api+json"},
+      JSON.stringify(response)
+    ];
+  });
+
+  return server;
+}
+
+test('logging in by clicking the submit button', function(assert) {
+  visit('/login');
 
   fillIn('.email', 'test@example.com');
   fillIn('.password', 'loginpass');
