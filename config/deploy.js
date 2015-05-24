7623d2788aabfed784f1769947fd901e27ca0128
Amos Chan <amosschan@gmail.com>
2015-05-24 14:44:49 -0400

    Production deployment settings

--- config/deploy.js
+++ config/deploy.js
@@ -16,5 +16,21 @@ module.exports = {
       privateKey: '~/.ssh/id_rsa',
       args: ['-av']
     }
+  },
+  production: {
+    store: {
+      type: 'redis',
+      host: 'mcac.church',
+      port: 6379
+    },
+    assets: {
+      gzip: false,
+      type: 'rsync',
+      dest: 'deploy@mcac.church:~/apps/mcac-js',
+      ssh: true,
+      port: 22,
+      privateKey: '~/.ssh/id_rsa',
+      args: ['-av']
+    }
   }
 };
