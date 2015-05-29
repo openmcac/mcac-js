f62b325c943edfa32872e6852283839945a70c86
Amos Chan <amosschan@gmail.com>
2015-05-28 23:19:52 -0400

    Only display audio player when applicable

--- tests/acceptance/sunday-test.js
+++ tests/acceptance/sunday-test.js
@@ -4,7 +4,7 @@ import Pretender from 'pretender';
 import { test, module } from 'qunit';
 import mockServer from '../helpers/server';
 
-var application;
+var application, server;
 
 var announcements = {
   "1": {
@@ -39,6 +39,7 @@ var announcements = {
 module('Acceptance: Sunday', {
   setup: function() {
     application = startApp();
+    server = mockServer();
   },
   teardown: function() {
     Ember.run(application, 'destroy');
@@ -46,7 +47,6 @@ module('Acceptance: Sunday', {
 });
 
 test('visiting /sunday', function(assert) {
-  var server = mockServer();
   server.get('/api/v1/announcements/:id', function(request) {
     var announcement = {
       "announcements": announcements[request.params.id]
@@ -75,11 +75,38 @@ test('visiting /sunday', function(assert) {
 
   andThen(function() {
     assert.equal(find('.bulletin-info .name').text(), 'Sunday Service');
+    assert.equal(find('audio').length, 0);
     assert.equal(find('.announcements li:nth-child(1)').text().trim(),
                  'This is the first announcement');
     assert.equal(find('.announcements li:nth-child(2)').text().trim(),
                  'This is the second announcement');
     assert.equal(find('.announcements li:nth-child(3)').text().trim(),
                  'This is the third announcement');
+  });
+});
+
+test('visiting a bulletin with audio', function(assert) {
+  server.get('/api/v1/sunday', function(request) {
+    var response = {
+      "bulletins": {
+        "id": "1",
+        "audioUrl": "http://example.com/audio.mp3",
+        "description": "This is a service bulletin.",
+        "name": "Sunday Service",
+        "serviceOrder": "This is the service order.",
+        "publishedAt": "2014-12-21T13:58:27-05:00",
+        "links": {
+          "group": "1",
+          "announcements": []
+        }
+      }
+    };
+    return [200, {"Content-Type": "application/vnd.api+json"}, JSON.stringify(response)];
+  });
+
+  visit('/sunday');
+
+  andThen(function() {
+    assert.equal(find('audio').length, 1);
   });
 });
