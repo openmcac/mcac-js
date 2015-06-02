64a36ca91704ef648f11cf95475ef198a067fdd0
Joseph <jyo.lewis@gmail.com>
2015-06-01 22:17:39 -0400

    Sort bulletins by publishedAt

--- app/controllers/bulletins/index.js
+++ app/controllers/bulletins/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
+  sortAscending: false,
+  sortProperties: ["publishedAt", "name"],
   actions: {
     remove: function(bulletin) {
       bulletin.destroyRecord();
