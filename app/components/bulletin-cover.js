e7afce3f562dd42854a88c9e42e768584cdecd4a
Joseph <jyo.lewis@gmail.com>
2015-06-22 21:28:37 -0400

    Listen to Resize event on bulletin-cover

--- app/components/bulletin-cover.js
+++ app/components/bulletin-cover.js
@@ -1,9 +1,14 @@
 import Ember from "ember";
+import ResizeAware from "ember-resize/mixins/resize-aware";
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(ResizeAware, {
   classNames: ["bulletin-cover"],
   resizeCover: function() {
     var screenHeight = Ember.$(window).height();
     Ember.$(".bulletin-cover").height(screenHeight);
-  }.on("didInsertElement")
+  }.on("didInsertElement"),
+  debouncedDidResize(width, height, evt) {
+    console.log(`Debounced Resize! ${width}x${height}`);
+    this.resizeCover();
+  }
 });
