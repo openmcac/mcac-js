4f033d5ef97fb0a0ebf1c32a5bb5f0c43fcc111c
Amos Chan <amosschan@gmail.com>
2015-06-23 22:38:54 -0400

    Ensure all relationships are async
    `ember watson:convert-ember-data-async-false-relationships`

--- app/models/announcement.js
+++ app/models/announcement.js
@@ -4,8 +4,12 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   description: DS.attr('string'),
   position: DS.attr('number'),
-  bulletin: DS.belongsTo('bulletin'),
-  post: DS.belongsTo('post'),
+  bulletin: DS.belongsTo('bulletin', {
+    async: false
+  }),
+  post: DS.belongsTo('post', {
+    async: false
+  }),
   url: DS.attr('string'),
   descriptionHtml: Ember.computed('description', function() {
     return marked(this.get('description') || '');
