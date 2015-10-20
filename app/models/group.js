f92d9a389730d964fa8dbefd0e65f44cbe8e5337
Amos Chan <amosschan@gmail.com>
2015-10-19 21:28:11 -0400

    Initial group home page
    Shows banner and description of the current group.

--- app/models/group.js
+++ app/models/group.js
@@ -4,5 +4,8 @@ export default DS.Model.extend({
   name: DS.attr("string"),
   slug: DS.attr("string"),
   about: DS.attr("string"),
-  bannerUrl: DS.attr("string")
+  bannerUrl: DS.attr("string"),
+  aboutHtml: Ember.computed("about", function() {
+    return marked(this.get("about") || "");
+  })
 });
