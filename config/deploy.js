ffa2d3a652c87f91f3d633704e96ad74f29516f8
Amos Chan <amosschan@gmail.com>
2016-01-09 21:31:12 -0500

    Upload index to redis via ssh tunnel

--- config/deploy.js
+++ config/deploy.js
@@ -28,12 +28,18 @@ module.exports = function(deployTarget) {
 
   if (deployTarget === 'staging') {
     ENV.build.environment = deployTarget;
-    ENV.redis.url = "redis://staging.mcac.church:6379/"
+    ENV.redis.host = "localhost"
     ENV.s3.accessKeyId = process.env.aws_access_key_id;
     ENV.s3.secretAccessKey = process.env.aws_secret_access_key;
     ENV.s3.bucket = "mcac-staging";
     ENV.s3.region = "us-east-1";
-    ENV.plugins = ["build", "revision-data", "s3", "redis"];
+
+    ENV["ssh-tunnel"] = {
+      username: "deploy",
+      host: "staging.mcac.church"
+    }
+
+    ENV.plugins = ["build", "revision-data", "s3", "ssh-tunnel", "redis"];
   }
 
   return ENV;
