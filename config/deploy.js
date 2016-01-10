d57750b6726fbd9dad2ca165b3d10a7522d43135
Amos Chan <amosschan@gmail.com>
2016-01-09 21:12:19 -0500

    Update deploy script to deploy staging app
     - Revision data
     - Upload to s3
     - Store in redis
    
    Still requires an SSH tunnel so we can close the public redis port.

--- config/deploy.js
+++ config/deploy.js
@@ -1,36 +1,60 @@
-/* jshint node: true */
+var VALID_DEPLOY_TARGETS = [ //update these to match what you call your deployment targets
+  'development',
+  'staging',
+  'production'
+];
 
-module.exports = {
-  staging: {
-    store: {
-      type: 'redis',
-      host: 'staging.mcac.church',
-      port: 6379
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {},
+    redis: {
+      allowOverwrite: true,
+      keyPrefix: 'basechurch:index'
     },
-    assets: {
-      gzip: false,
-      type: 'rsync',
-      dest: 'deploy@staging.mcac.church:~/apps/mcac-js',
-      ssh: true,
-      port: 22,
-      privateKey: '~/.ssh/id_rsa',
-      args: ['-av']
+    s3: {
+      prefix: 'app'
     }
-  },
-  production: {
-    store: {
-      type: 'redis',
-      host: 'mcac.church',
-      port: 6379
-    },
-    assets: {
-      gzip: false,
-      type: 'rsync',
-      dest: 'deploy@mcac.church:~/apps/mcac-js',
-      ssh: true,
-      port: 22,
-      privateKey: '~/.ssh/id_rsa',
-      args: ['-av']
-    }
+  };
+
+  if (VALID_DEPLOY_TARGETS.indexOf(deployTarget) === -1) {
+    throw new Error('Invalid deployTarget ' + deployTarget);
   }
-};
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    ENV.redis.url = process.env.REDIS_URL || 'redis://0.0.0.0:6379/';
+    ENV.plugins = ['build', 'redis']; // only care about deploying index.html into redis in dev
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = deployTarget;
+    ENV.redis.url = "redis://staging.mcac.church:6379/"
+    ENV.s3.accessKeyId = process.env.aws_access_key_id;
+    ENV.s3.secretAccessKey = process.env.aws_secret_access_key;
+    ENV.s3.bucket = "mcac-staging";
+    ENV.s3.region = "us-east-1";
+    ENV.plugins = ["build", "revision-data", "s3", "redis"];
+  }
+
+  return ENV;
+
+  /* Note: a synchronous return is show above, but ember-cli-deploy
+   * does support returning a promise, in case you need to get any of
+   * your configuration asynchronously. e.g.
+   *
+   *    var Promise = require('ember-cli/lib/ext/promise');
+   *    return new Promise(function(resolve, reject){
+   *      var exec = require('child_process').exec;
+   *      var command = 'heroku config:get REDISTOGO_URL --app my-app-' + deployTarget;
+   *      exec(command, function (error, stdout, stderr) {
+   *        ENV.redis.url = stdout.replace(/\n/, '').replace(/\/\/redistogo:/, '//:');
+   *        if (error) {
+   *          reject(error);
+   *        } else {
+   *          resolve(ENV);
+   *        }
+   *      });
+   *    });
+   *
+   */
+}
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,8 +2,38 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  var env = EmberApp.env() || "development";
+  var isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1;
+  var fingerprintOptions = {
+    enabled: true,
+    extensions: ['js', 'css', 'png', 'jpg', 'gif']
+  };
+
+  switch (env) {
+    case "development":
+      fingerprintOptions.prepend = "http://localhost:4200/";
+      break;
+    case "staging":
+      fingerprintOptions.prepend = "https://s3.amazonaws.com/mcac-staging/app/";
+      break;
+  }
+
   var app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: fingerprintOptions,
+    emberCLIDeploy: {
+      runOnPostBuild: (env === "development") ? "development-postbuild" : false,
+      configFile: "config/deploy.js",
+      shouldActivate: true
+    },
+    sourcemaps: {
+      enabled: !isProductionLikeBuild
+    },
+    minifyCSS: {
+      enabled: isProductionLikeBuild
+    },
+    minifyJS: {
+      enabled: isProductionLikeBuild
+    }
   });
 
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
