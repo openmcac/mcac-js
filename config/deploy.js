2cc9d1d21269d4e8dcefda94547722543aa0ce1a
Amos Chan <amosschan@gmail.com>
2016-01-13 20:46:45 -0500

    Clean up deploy script

--- config/deploy.js
+++ config/deploy.js
@@ -1,5 +1,4 @@
-var VALID_DEPLOY_TARGETS = [ //update these to match what you call your deployment targets
-  'development',
+var VALID_DEPLOY_TARGETS = [
   'staging',
   'production'
 ];
@@ -20,14 +19,8 @@ module.exports = function(deployTarget) {
     throw new Error('Invalid deployTarget ' + deployTarget);
   }
 
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    ENV.redis.url = process.env.REDIS_URL || 'redis://0.0.0.0:6379/';
-    ENV.plugins = ['build', 'redis']; // only care about deploying index.html into redis in dev
-  }
-
   if (deployTarget === 'staging') {
-    ENV.build.environment = deployTarget;
+    ENV.build.environment = "staging";
     ENV.redis.host = "localhost"
     ENV.s3.accessKeyId = process.env.aws_access_key_id;
     ENV.s3.secretAccessKey = process.env.aws_secret_access_key;
@@ -43,24 +36,4 @@ module.exports = function(deployTarget) {
   }
 
   return ENV;
-
-  /* Note: a synchronous return is show above, but ember-cli-deploy
-   * does support returning a promise, in case you need to get any of
-   * your configuration asynchronously. e.g.
-   *
-   *    var Promise = require('ember-cli/lib/ext/promise');
-   *    return new Promise(function(resolve, reject){
-   *      var exec = require('child_process').exec;
-   *      var command = 'heroku config:get REDISTOGO_URL --app my-app-' + deployTarget;
-   *      exec(command, function (error, stdout, stderr) {
-   *        ENV.redis.url = stdout.replace(/\n/, '').replace(/\/\/redistogo:/, '//:');
-   *        if (error) {
-   *          reject(error);
-   *        } else {
-   *          resolve(ENV);
-   *        }
-   *      });
-   *    });
-   *
-   */
 }
