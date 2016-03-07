d252a3a67a6fc845fb55a6014da291b4913fd5e3
Amos Chan <amosschan@gmail.com>
2016-03-07 17:49:46 -0500

    Add transitions between bulletins
    Move the bulletin to the right when navigating to older bulletin and
    left to a newer bulletin.

--- app/transitions.js
+++ app/transitions.js
@@ -1,2 +1,49 @@
+import Bulletin from "mcac/models/bulletin";
+
 export default function(){
+  this.transition(
+    this.toValue(function(toValue, fromValue) {
+      if (!transitionBetweenBulletins(toValue, fromValue)) {
+        return false
+      }
+
+      if (toValue.publishedEarlierThan(fromValue)) {
+        return true;
+      }
+
+      return false;
+    }),
+    this.use("toRight")
+  );
+
+  this.transition(
+    this.toValue(function(toValue, fromValue) {
+      if (!transitionBetweenBulletins(toValue, fromValue)) {
+        return false
+      }
+
+      if (!toValue.publishedEarlierThan(fromValue)) {
+        return true;
+      }
+
+      return false;
+    }),
+    this.use("toLeft")
+  );
+
+  this.transition(
+    this.toRoute('sunday'),
+    this.use('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('bulletin/sunday'),
+    this.toRoute('group'),
+    this.use('toRight'),
+    this.debug()
+  );
 };
+
+function transitionBetweenBulletins(toValue, fromValue) {
+  return toValue instanceof Bulletin && fromValue instanceof Bulletin;
+}
