fdf7111983e97687f845ddd5a4125401317248a9
Amos Chan <amosschan@gmail.com>
2016-03-30 23:18:48 -0400

    Refactor edit bulletin route

--- app/routes/bulletin/edit.js
+++ app/routes/bulletin/edit.js
@@ -1,23 +1,24 @@
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   defaultAnnouncementsService: Ember.inject.service("default-announcements"),
   model: function() {
     const bulletin = this.modelFor("bulletin");
-
-    bulletin.get("announcements").then(announcements => {
-      if (announcements.get("length") > 0) return;
-
-      const groupId = this.modelFor("group").get("id");
-      const defaultAnnouncements =
-        this.get("defaultAnnouncementsService").process(groupId);
-
-      defaultAnnouncements.then(defaults => {
-        defaults.forEach(a => announcements.addObject(a))
-      });
-    });
-
+    this.get("populateDefaultAnnouncementsTask").perform(bulletin);
     return bulletin;
-  }
+  },
+  populateDefaultAnnouncementsTask: task(function * (bulletin) {
+    const announcements = yield bulletin.get("announcements");
+    if (announcements.get("length") > 0) {
+      return;
+    }
+
+    const groupId = this.modelFor("group").get("id");
+    const defaultAnnouncements =
+      yield this.get("defaultAnnouncementsService").process(groupId);
+
+    defaultAnnouncements.forEach(a => announcements.addObject(a));
+  })
 });
