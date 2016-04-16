d9723c2923b955769fc76f412e7a3dfe457e95d3
Amos Chan <amosschan@gmail.com>
2016-04-16 15:31:19 -0400

    More group/index UI changes

--- app/components/post-view.js
+++ app/components/post-view.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 const TIMEZONE = "America/Montreal";
-const DATE_FORMAT = "MMMM D YYYY, h:mm a";
+const DATE_FORMAT = "MMMM D [at] h:mma";
 
 export default Ember.Component.extend({
   classNames: ["post-view"],
