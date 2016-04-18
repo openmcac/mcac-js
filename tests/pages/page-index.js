b4168ab43145dd0f6450a9b62aad07a38437b2bb
Amos Chan <amosschan@gmail.com>
2016-04-17 22:45:12 -0400

    Refine styles for pages and posts

--- tests/pages/page-index.js
+++ tests/pages/page-index.js
@@ -1,13 +1,9 @@
 import PageObject from '../page-object';
+import postView from "mcac/tests/pages/components/post-view";
 
-const { visitable, text } = PageObject;
+const { visitable } = PageObject;
 
 export default PageObject.create({
   visit: visitable("/:groupSlug/:slug"),
-  title: text(selector("title")),
-  content: text(selector("content"))
+  page: postView
 });
-
-function selector(s) {
-  return `*[data-auto-id='page-${s}']`;
-}
