c4c57abe6d7f8e91c030be28d7fbb6332a33ede7
Amos Chan <amosschan@gmail.com>
2016-05-02 22:50:19 -0400

    Fix cloudinaryFetch specs

--- tests/unit/helpers/cloudinary-fetch-test.js
+++ tests/unit/helpers/cloudinary-fetch-test.js
@@ -1,10 +1,15 @@
 import { cloudinaryFetch } from 'mcac/helpers/cloudinary-fetch';
 import { module, test } from 'qunit';
+import ENV from "mcac/config/environment";
 
 module('Unit | Helper | cloudinary fetch');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = cloudinaryFetch([42]);
-  assert.ok(result);
+  const width = 100;
+  const url = "http://www.test.com/image.jpg";
+  const args = { width: width, url: url };
+
+  const result = cloudinaryFetch(null, args);
+  assert.equal(result, `https://res.cloudinary.com/${ENV.cloudinary.cloudName}/image/fetch/w_${width}/${url}`);
 });
