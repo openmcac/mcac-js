26e6c8c13e91050e224388082c3be0550cc3890a
Amos Chan <amosschan@gmail.com>
2016-06-30 22:43:26 -0400

    Specs for sermon notes

---
+++ tests/integration/components/sermon-notes-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import page from "mcac/tests/pages/components/sermon-notes";
+
+moduleForComponent('sermon-notes', 'Integration | Component | sermon notes', {
+  integration: true,
+  afterEach: () => page.removeContext()
+});
+
+test('with sermon notes', function(assert) {
+  page.setContext(this).
+    render(hbs`{{sermon-notes notes="<div id='test'>hello world</div>"}}`);
+
+  assert.equal(page.title, "Notes");
+  assert.equal(page.notes, "hello world");
+  assert.notOk(page.noNotesIndicatorShown());
+});
+
+test('without sermon notes', function(assert) {
+  page.setContext(this).render(hbs`{{sermon-notes}}`);
+
+  assert.equal(page.title, "Notes");
+  assert.ok(page.noNotesIndicatorShown());
+});
---
+++ tests/pages/components/sermon-notes.js
@@ -0,0 +1,16 @@
+import PageObject from '../../page-object';
+
+const { text } = PageObject;
+
+export default PageObject.create({
+  title: text(selector("title")),
+  content: text(selector("content")),
+  noNotesIndicatorShown() {
+    return $.find(selector("no-notes")).length > 0;
+  },
+  notes: text(selector("notes"))
+});
+
+function selector(s) {
+  return `*[data-auto-id="sermon-notes"] *[data-auto-id="${s}"]`;
+}
