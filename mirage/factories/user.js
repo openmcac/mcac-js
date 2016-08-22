3f06c1558a46e1eb86affad139f2c4a0802c5545
Amos Chan <amosschan@gmail.com>
2016-08-22 19:14:25 -0400

    Add user mirage model

--- mirage/factories/user.js
+++ mirage/factories/user.js
@@ -1,6 +1,6 @@
-import Mirage, { faker } from "ember-cli-mirage";
+import { Factory, faker } from "ember-cli-mirage";
 
-export default Mirage.Factory.extend({
+export default Factory.extend({
   name() {
     return `${faker.name.firstName} ${faker.name.lastName}`;
   },
