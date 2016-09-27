76411bce9a4f9ab89d8cba0145cda1d5dd0fab47
Amos Chan <amosschan@gmail.com>
2016-09-27 16:23:08 -0400

    Manually parse page number from URL

--- app/controllers/group/index.js
+++ app/controllers/group/index.js
@@ -3,7 +3,17 @@ import Pagination from 'ember-cli-jsonapi-pagination/mixins/controllers/jsonapi-
 
 export default Ember.Controller.extend(Pagination, {
   totalPages: Ember.computed('size', 'number', 'model.posts.[]', function() {
-    const lastPage = new URL(this.get("posts.links.last"));
-    return parseInt(lastPage.searchParams.get("page[number]"));
+    return parseInt(getParameterByName("page[number]", this.get("posts.links.last")));
   })
 });
+
+function getParameterByName(name, url) {
+  name = encodeURIComponent(name);
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  const results = regex.exec(url);
+
+  if (!results) return null;
+  if (!results[2]) return "";
+
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
