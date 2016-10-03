c8f629267d7ff57e07f9e7fdf1a14b917302b1da
Amos Chan <amosschan@gmail.com>
2016-10-03 18:36:09 -0400

    Double quote all the things ember cli build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,13 +1,13 @@
 /*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
   var env = EmberApp.env() || "development";
-  var isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1;
+  var isProductionLikeBuild = ["production", "staging"].indexOf(env) > -1;
   var fingerprintOptions = {
     enabled: true,
-    extensions: ['js', 'css', 'png', 'jpg', 'gif']
+    extensions: ["js", "css", "png", "jpg", "gif"]
   };
 
   switch (env) {
@@ -42,33 +42,33 @@ module.exports = function(defaults) {
     dotEnv: {
       clientAllowedKeys: ["CLOUDINARY_CLOUD_NAME"],
       path: {
-        development: '.env',
-        test: '.env.test',
-        production: '.env.production'
+        development: ".env",
+        test: ".env.test",
+        production: ".env.production"
       }
     }
   });
 
-  app.import('bower_components/tether/dist/js/tether.js');
-  app.import('bower_components/tether/dist/css/tether.css');
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-  app.import('bower_components/moment/moment.js');
-  app.import('bower_components/moment-timezone/builds/moment-timezone-with-data-2010-2020.js');
-  app.import('bower_components/bootstrap-markdown/js/bootstrap-markdown.js');
-  app.import('bower_components/bootstrap-markdown/css/bootstrap-markdown.min.css');
-  app.import('bower_components/marked/lib/marked.js');
-  app.import('bower_components/pace/pace.js');
-  app.import('bower_components/pace/themes/purple/pace-theme-center-simple.css');
-  app.import('bower_components/ember-uploader/dist/ember-uploader.named-amd.js');
-  app.import('vendor/fontello/css/fontello.css');
+  app.import("bower_components/tether/dist/js/tether.js");
+  app.import("bower_components/tether/dist/css/tether.css");
+  app.import("bower_components/bootstrap/dist/js/bootstrap.js");
+  app.import("bower_components/bootstrap/dist/css/bootstrap.css");
+  app.import("bower_components/moment/moment.js");
+  app.import("bower_components/moment-timezone/builds/moment-timezone-with-data-2010-2020.js");
+  app.import("bower_components/bootstrap-markdown/js/bootstrap-markdown.js");
+  app.import("bower_components/bootstrap-markdown/css/bootstrap-markdown.min.css");
+  app.import("bower_components/marked/lib/marked.js");
+  app.import("bower_components/pace/pace.js");
+  app.import("bower_components/pace/themes/purple/pace-theme-center-simple.css");
+  app.import("bower_components/ember-uploader/dist/ember-uploader.named-amd.js");
+  app.import("vendor/fontello/css/fontello.css");
 
-  var mergeTrees = require('broccoli-merge-trees');
-  var pickFiles = require('broccoli-static-compiler');
-  var customFonts = pickFiles('vendor/fontello/font', {
-    srcDir: '/',
-    files: ['**/*'],
-    destDir: '/font'
+  var mergeTrees = require("broccoli-merge-trees");
+  var pickFiles = require("broccoli-static-compiler");
+  var customFonts = pickFiles("vendor/fontello/font", {
+    srcDir: "/",
+    files: ["**/*"],
+    destDir: "/font"
   });
 
   return mergeTrees([app.toTree(), customFonts]);
