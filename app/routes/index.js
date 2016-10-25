fd0ef37511de60d16160476573f205f9b02b6863
Joseph <jyo.lewis@gmail.com>
2016-10-24 21:50:06 -0400

    Displays groups dynamically

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.query('group', {
-     'filter[id]': 1
-   }).then((groups) => {
-     return groups.objectAt(0);
-   });
+    return this.get('store').findAll("group");
   }
 });
