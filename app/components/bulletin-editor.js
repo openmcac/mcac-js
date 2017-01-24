3985546d88bfe9b9d3009241e1dee7e7fa161ff3
Amos Chan <amosschan@gmail.com>
2017-01-23 21:51:55 -0500

    Make bulletin editor load

--- app/components/bulletin-editor.js
+++ app/components/bulletin-editor.js
@@ -1,16 +1,5 @@
 import Ember from 'ember';
-import EmberValidations from 'ember-validations';
 
-export default Ember.Component.extend(EmberValidations, {
-  validations: {
-    "bulletin.name": {
-      presence: true
-    },
-    "bulletin.publishedAt": {
-      presence: true
-    }
-  },
-  disableSaveButton: Ember.computed("isValid", function() {
-    return !this.get("isValid");
-  })
+export default Ember.Component.extend({
+  disableSaveButton: false
 });
