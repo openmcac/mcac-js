b2c15b06b2b8d0fc25df307aa236e6681c3911d0
Amos Chan <amosschan@gmail.com>
2017-01-30 21:54:16 -0500

    Add spec to create full bulletin

--- tests/acceptance/group/bulletins/new-test.js
+++ tests/acceptance/group/bulletins/new-test.js
@@ -13,15 +13,61 @@ moduleForAcceptance('Acceptance | group/bulletins/new', {
   }
 });
 
-test('visiting /english-service/bulletins/new', function(assert) {
+test('creating a new bulletin', function(assert) {
+  const existingAnnouncements  = [
+    server.create("announcement", { id: 1 }),
+    server.create("announcement", { id: 2 }),
+    server.create("announcement", { id: 3 })
+  ];
   const group = server.create("group");
   const bulletin = server.create("bulletin", { group });
 
   authenticateSession(application, sessionData);
 
-  page.visit({ group_slug: bulletin.group.slug });
+  page.
+    visit({ group_slug: bulletin.group.slug }).
+    name("my bulletin").
+    serviceOrder("my service order").
+    publishedAt("03/23/2000 9:30 AM").
+    series("sermon series").
+    sermonName("sermon name").
+    speaker("sermon speaker").
+    tags("tag1, tag2, tag3").
+    notes("sermon notes").
+    appendAnnouncement();
+
+  page.announcements(3).description("new announcement");
+
+  page.submit();
 
   andThen(function() {
-    assert.equal(currentURL(), `/${group.slug}/bulletins/new`);
+    const createdBulletin = server.db.bulletins[server.db.bulletins.length - 1];
+    assert.equal(createdBulletin.name, "my bulletin");
+    assert.equal(createdBulletin.serviceOrder, "my service order");
+    assert.deepEqual(new Date(createdBulletin.publishedAt),
+                     new Date(2000, 2, 23, 9, 30));
+
+    const createdSermon = server.db.sermons.find(createdBulletin.sermonId);
+    assert.equal(createdSermon.series, "sermon series");
+    assert.equal(createdSermon.name, "sermon name");
+    assert.equal(createdSermon.speaker, "sermon speaker");
+    assert.equal(createdSermon.tags, "tag1, tag2, tag3");
+    assert.equal(createdSermon.notes, "sermon notes");
+
+    const createdAnnouncements =
+      server.db.announcements.where({ bulletinId: createdBulletin.id });
+
+    assert.equal(createdAnnouncements[0].description,
+                 existingAnnouncements[0].description);
+    assert.equal(createdAnnouncements[1].description,
+                 existingAnnouncements[1].description);
+    assert.equal(createdAnnouncements[2].description,
+                 existingAnnouncements[2].description);
+    assert.equal(createdAnnouncements[3].description, "new announcement");
+
+    assert.equal(existingAnnouncements.length, 3);
+    assert.equal(createdAnnouncements.length, 4);
+
+    assert.equal(currentURL(), "/dashboard");
   });
 });
