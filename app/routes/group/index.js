bf14bcfa56bdba1eba5c30047e6c48a36a8e8377
Amos Chan <amosschan@gmail.com>
2017-04-19 15:37:51 -0400

    Meta tags (#177)
    * Add OpenGraph tags for homepage
    
    * Head tags for groups
    
    * Remove unused file

--- app/routes/group/index.js
+++ app/routes/group/index.js
@@ -30,5 +30,31 @@ export default Ember.Route.extend(Pagination, {
   setupController(controller, model) {
     controller.set("posts", model.posts);
     controller.set("group", model.group);
+  },
+	headTags() {
+    const model = this.modelFor(this.routeName);
+
+    return [{
+      type: 'meta',
+      tagId: 'meta-og-title',
+      attrs: {
+        property: 'og:title',
+        content: `${model.group.get("name")} | Montreal Chinese Alliance Church`
+      }
+    }, {
+      type: 'meta',
+      tagId: 'meta-og-description',
+      attrs: {
+        property: 'og:description',
+        content: `${model.group.get("shortDescription")}`
+      }
+    }, {
+      type: 'meta',
+      tagId: 'meta-og-image',
+      attrs: {
+        property: 'og:image',
+        content: `https://res.cloudinary.com/fisherhall/image/fetch/w_1200,h_630,c_pad,b_white/${model.group.get("bannerUrl")}`
+      }
+    }];
   }
 });
