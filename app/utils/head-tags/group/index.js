acd62cf264bff45a0dd996898fd60c4972cdb741
Amos Chan <amosschan@gmail.com>
2017-04-19 18:20:57 -0400

    Add canonical url for groups

--- app/utils/head-tags/group/index.js
+++ app/utils/head-tags/group/index.js
@@ -1,4 +1,8 @@
+import ENV from "mcac/config/environment";
+
 export default function(group) {
+  const canonicalUrl = `${ENV["DOMAIN"]}/${group.get("slug")}`;
+
   return [{
     type: 'meta',
     tagId: 'meta-og-title',
@@ -18,7 +22,21 @@ export default function(group) {
     tagId: 'meta-og-image',
     attrs: {
       property: 'og:image',
-      content: `https://res.cloudinary.com/fisherhall/image/fetch/w_1200,h_630,c_pad,b_white/${group.get("bannerUrl")}`
+      content: `https://res.cloudinary.com/${ENV["CLOUDINARY_CLOUD_NAME"]}/image/fetch/w_1200,h_630,c_pad,b_white/${group.get("bannerUrl")}`
+    }
+  }, {
+    type: 'meta',
+    tagId: 'meta-og-url',
+    attrs: {
+      property: 'og:url',
+      content: canonicalUrl
+    }
+  }, {
+    type: 'link',
+    tagId: 'link-canonical',
+    attrs: {
+      rel: 'canonical',
+      href: canonicalUrl
     }
   }];
 };
--- tests/unit/utils/head-tags/group/index-test.js
+++ tests/unit/utils/head-tags/group/index-test.js
@@ -1,6 +1,7 @@
 import headTags from 'mcac/utils/head-tags/group/index';
 import { module, test } from 'qunit';
 import Ember from 'ember';
+import ENV from "mcac/config/environment";
 
 module('Unit | Utility | head tags/group/index');
 
@@ -8,7 +9,8 @@ test("it populates opengraph tags", function(assert) {
   const group = Ember.Object.create({
     name: "My Group",
     shortDescription: "My short description",
-    bannerUrl: "https://example.com/banner.jpg"
+    bannerUrl: "https://example.com/banner.jpg",
+    slug: "test-group"
   });
 
   const actualTags = headTags(group);
@@ -32,7 +34,21 @@ test("it populates opengraph tags", function(assert) {
     tagId: 'meta-og-image',
     attrs: {
       property: 'og:image',
-      content: "https://res.cloudinary.com/fisherhall/image/fetch/w_1200,h_630,c_pad,b_white/https://example.com/banner.jpg"
+      content: `https://res.cloudinary.com/${ENV["CLOUDINARY_CLOUD_NAME"]}/image/fetch/w_1200,h_630,c_pad,b_white/https://example.com/banner.jpg`
+    }
+  }, {
+    type: 'meta',
+    tagId: 'meta-og-url',
+    attrs: {
+      property: 'og:url',
+      content: `${ENV["DOMAIN"]}/test-group`
+    }
+  }, {
+    type: 'link',
+    tagId: 'link-canonical',
+    attrs: {
+      rel: 'canonical',
+      href: `${ENV["DOMAIN"]}/test-group`
     }
   }];
 
