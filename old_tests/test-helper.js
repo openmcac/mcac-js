0cd211c22c3abd414c7eea4ba70a4e35964912ac
Amos Chan <amosschan@gmail.com>
2017-04-22 16:18:37 -0400

    Remove old tests

--- old_tests/test-helper.js
+++
@@ -1,6 +0,0 @@
-import resolver from './helpers/resolver';
-import registerSelectHelper from './helpers/register-select-helper';
-import { setResolver } from 'ember-qunit';
-
-setResolver(resolver);
-registerSelectHelper();
